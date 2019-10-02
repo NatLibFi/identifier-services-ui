b95c633a1f5625edd2ed4b222db0bd42a79539b1
Shrestha Sanjog R <srshrest@lx9-9815-007.lib.helsinki.fi>
2019-10-02 14:47:02 +0300

    disable when not processed

--- src/frontend/components/publishersRequests/publisherRequest.js
+++ src/frontend/components/publishersRequests/publisherRequest.js
@@ -98,7 +98,7 @@ export default connect(mapStateToProps, actions)(reduxForm({
 			case 'new':
 				return (
 					<ButtonGroup color="primary" aria-label="outlined primary button group">
-						<Button variant="outlined" color="primary" onClick={handleAccept}>Accept</Button>
+						<Button disabled={publisherRequest.backgroundProcessingState !== 'processed'} variant="outlined" color="primary" onClick={handleAccept}>Accept</Button>
 						<Button variant="outlined" style={{color: 'red'}} onClick={handleRejectClick}>Reject</Button>
 					</ButtonGroup>
 				);
